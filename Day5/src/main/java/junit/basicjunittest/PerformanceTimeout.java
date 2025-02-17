8ae17925d780e2a33ccff5720ce3675c57a06467
bhavyuh15 <bhavyadatey15@gmail.com>
2025-02-17 10:43:48 +0530

    [Bhavya] added basicjunittest folder in Day5 branch

---
+++ Day5/src/main/java/junit/basicjunittest/PerformanceTimeout.java
@@ -0,0 +1,10 @@
+package junit.basicjunittest;
+
+public class PerformanceTimeout {
+    public static String longRunningTask() throws InterruptedException {
+        Thread.sleep(3000); // Simulating a long task (3 seconds)
+        return "Task Completed";
+    }
+}
+
+
